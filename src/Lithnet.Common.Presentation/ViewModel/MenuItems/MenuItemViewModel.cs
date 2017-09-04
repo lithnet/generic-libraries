9f93748ec0a8e044dc092c3ca0f7c0728804f47c
ryannewington <ryan@lithiumblue.com>
2017-09-04 12:51:19 +1000

    Adds support for menu item view models

---
+++ src/Lithnet.Common.Presentation/ViewModel/MenuItems/MenuItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using PropertyChanged;
+
+namespace Lithnet.Common.Presentation
+{
+    [ImplementPropertyChanged]
+    public class MenuItemViewModel : MenuItemViewModelBase
+    {
+        public DelegateCommand Command { get; set; }
+
+        public BitmapImage Icon { get; set; }
+
+        public MenuItemViewModel()
+        {
+            this.MenuItems = new ObservableCollection<MenuItemViewModelBase>();
+        }
+
+        public string Header { get; set; }
+
+        public ObservableCollection<MenuItemViewModelBase> MenuItems { get; private set; }
+    }
+}
---
+++ src/Lithnet.Common.Presentation/ViewModel/MenuItems/SeparatorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using PropertyChanged;
+
+namespace Lithnet.Common.Presentation
+{
+    public class SeparatorViewModel : MenuItemViewModelBase
+    {
+    }
+}
